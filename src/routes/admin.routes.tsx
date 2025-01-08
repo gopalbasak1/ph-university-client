4d9ba03eda692da3b49ed1215b50a1dc4e61bab5
gopalbasak1 <gopalbasak1223@gmail.com>
2025-01-08 13:09:23 +0600

    ph university client setup and routes handle

---
+++ src/routes/admin.routes.tsx
@@ -0,0 +1,123 @@
+import { ReactNode } from "react";
+import { NavLink } from "react-router-dom";
+
+type TRoute = {
+  path: string;
+  element: ReactNode;
+};
+
+type TSidebarItem = {
+  key: string;
+  label: ReactNode;
+  children?: TSidebarItem[];
+};
+
+const adminPaths = [
+  {
+    name: "Dashboard",
+    path: "dashboard",
+    element: "ADMIN_DASHBOARD",
+  },
+  {
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: "CREATE_ADMIN",
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: "CREATE_FACULTY",
+      },
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: "CREATE_STUDENT",
+      },
+    ],
+  },
+];
+
+//Programmatically Way
+
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+  if (item.children) {
+    item.children.forEach((child) => {
+      acc.push({
+        path: child.path,
+        element: child.element,
+      });
+    });
+  }
+  return acc;
+}, []);
+
+export const adminSidebarItems = adminPaths.reduce(
+  (acc: TSidebarItem[], item) => {
+    if (item.path && item.name) {
+      acc.push({
+        key: item.name,
+        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
+      });
+    }
+    if (item.children) {
+      acc.push({
+        key: item.name,
+        label: item.name,
+        children: item.children.map((child) => ({
+          key: child.name,
+          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
+        })),
+      });
+    }
+    return acc; // Ensure the accumulator is returned
+  },
+  [] // Initial value of the accumulator
+);
+
+// export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+//   if (item.path && item.element) {
+//     acc.push({
+//       path: item.path,
+//       element: item.element,
+//     });
+//   }
+//   if (item.children) {
+//     item.children.forEach((child) => {
+//       acc.push({
+//         path: child.path,
+//         element: child.element,
+//       });
+//     });
+//   }
+//   return acc;
+// }, []);
+
+// ! Hard Coded
+
+// export const adminPaths = [
+//   {
+//     path: "dashboard",
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "create-student",
+//     element: <CreateStudent />,
+//   },
+//   {
+//     path: "create-admin",
+//     element: <CreateAdmin />,
+//   },
+//   {
+//     path: "create-faculty",
+//     element: <CreateFaculty />,
+//   },
+// ];
